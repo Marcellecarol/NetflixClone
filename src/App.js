43e111e9ec12fe24fc06324a1dc897580f0f1832
Marcellecarol <marcellecarolina15.ufrj@gmail.com>
2024-06-22 15:54:20 -0300

    atualização3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate, Link, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
 import './App.css';
 import tmdb from "./tmdb";
 import MovieRow from "./componentes/MovieRow";
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -6,12 +6,12 @@ const Header = ({ blackHeader, onLogout, user }) => {
   return (
     <header className={`header ${blackHeader ? 'black' : ''}`}>
       <div className="header--logo">
-        <Link to="/profile">
+        <Link to={user ? "/profile" : "/login"}>
           <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" />
         </Link>
       </div>
       <div className="header--user">
-        {user && (
+        {user ? (
           <>
             <Link to="/profile">
               <img className="header--avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Usuário" />
@@ -19,6 +19,8 @@ const Header = ({ blackHeader, onLogout, user }) => {
             <span className="header--username">{user}</span>
             <button className="header--logout" onClick={onLogout}>Logout</button>
           </>
+        ) : (
+          <Link to="/login" className="header--login">Login</Link>
         )}
       </div>
     </header>
